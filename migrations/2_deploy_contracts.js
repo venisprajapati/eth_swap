00609fd1a87c032996f56d22b25c968c106dc5d6
Venis Prajapati <59964517+venisprajapati@users.noreply.github.com>
2021-05-21 21:30:44 +0530

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+const Token = artifacts.require("Token");
+// eslint-disable-next-line no-undef
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function (deployer) {
+    // Deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed();
+
+    // Deploy EthSwap
+    await deployer.deploy(EthSwap, token.address);
+    const ethSwap = await EthSwap.deployed();
+
+    // Transfer all tokens to EthSwap (1 million)
+    await token.transfer(ethSwap.address, '1000000000000000000000000');
+    
+};
---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Dapp University
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <h1>Hello World!</h1>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
